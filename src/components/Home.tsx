97b8ea3c8ad37a8e834679888f2b24e7f849d845
Anders Nilsson <nilssonanders79@gmail.com>
2023-11-17 10:43:16 +0100

    more fixing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,10 @@ import "./Home.css";
 
 const Home = () => (
   <div className="home fb-ig">
-    <h2>Welcome to the Home Page</h2>
-    <p>This is the home page of your React application.</p>
+    <h2>&lt;- Pick an example</h2>
+    <p>
+      The examples in the left panel demonstrates how the @qlik/embed-react library can be used in embedding scenarios
+    </p>
   </div>
 );
 
